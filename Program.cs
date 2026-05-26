f42d2808da60bd81abc0293bbb4d05762a2eeb83
rodrigueszkkk <kaikyauthentic@gmail.com>
2026-05-25 22:19:06 -0300

    fix again

--- Program.cs
+++ Program.cs
@@ -18,11 +18,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
